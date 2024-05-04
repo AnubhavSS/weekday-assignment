d461c45223a04ed5ec7987cb17d6084902799398
Anubhav Singh <anubhavsssingh4@gmail.com>
2024-05-04 18:05:52 +0530

    data fetching from api done

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+
+
+const Cards = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        Lizard
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        Lizards are a widespread group of squamate reptiles, with over 6,000
+        species, ranging across all continents except Antarctica
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Share</Button>
+    </CardActions>
+  </Card>
+  )
+}
+
+export default Cards
---
+++ src/components/Hero.jsx
@@ -0,0 +1,51 @@
+import React,{useEffect,useState} from "react";
+import Cards from "./Card";
+
+const Hero = () => {
+
+ //function to fetch data from API   
+ async function fetchData() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const body = JSON.stringify({
+      limit: 10,
+      offset: 0,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+
+    return fetch(
+      "https://api.weekday.technology/adhoc/getSampleJdJSON",
+      requestOptions
+    )
+      .then((response) => response.text())
+    //   .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    async function fetchDataAndLogResponse() {
+      try {
+        const response = await fetchData();
+        console.log(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchDataAndLogResponse();
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
+  
+
+  return (
+    <div>
+      Hero
+      <Cards />
+    </div>
+  );
+};
+
+export default Hero;
